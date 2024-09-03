69d0f68943c48338384408eb503f9f8cefa2a1d6
Zcy. <157526994+4ozCy@users.noreply.github.com>
2024-09-03 11:50:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -594,6 +594,7 @@ async function handleButton(interaction) {
       await channel.delete();
     }, 5000);
   }
+}
   
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);

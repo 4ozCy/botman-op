40d5ad2f033eb0e2fa340b42aad88e95757182ef
Zcy. <157526994+4ozCy@users.noreply.github.com>
2024-09-11 15:13:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -288,16 +288,15 @@ const commands = [
 (async () => {
   const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
-  try {
-    await rest.put(
-      Routes.applicationGuildCommands(process.env.CLIENT_ID),
-      { body: commands }
-    );
-    console.log('Successfully registered (/) application commands.');
-  } catch (error) {
-    console.error('Error registering application commands:', error);
-  }
-})();
+try {
+  console.log('Started refreshing application (/) commands.');
+
+  await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+
+  console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+  console.error(error);
+}
 
 client.on('messageCreate', async (message) => {
   if (message.author.bot) return;

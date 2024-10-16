4ca9efea44bda2c828f5043cecd783c67770495f
Zcy. <157526994+4ozCy@users.noreply.github.com>
2024-10-17 01:27:48 +0700

    Create K.js

---
+++ K.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// made by: @n.int
+while (!inLoveWith(you)) {
+  console.log("Initializing attraction...");
+}
+console.log("Process complete: I'm in love!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+

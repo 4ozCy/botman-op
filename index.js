40220839b129ad93ceb276f33402b61a4317a665
Zcy. <157526994+4ozCy@users.noreply.github.com>
2025-05-14 23:13:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -369,7 +369,6 @@ if (commandName === 'warn') {
     for (let i = 0; i < length; i++) {
       const randomIndex = Math.floor(Math.random() * characters.length);
       randomString += characters[randomIndex];
-    )
 
     await interaction.reply({ content: `\`${randomString}\``, ephemeral: false });
 
